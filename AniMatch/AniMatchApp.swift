e6b1aecb313f7589541a01e3384f6e378cdf389a
Tara L <taralim826@gmail.com>
2024-03-28 15:14:39 +0800

    Initial Commit

---
+++ AniMatch/AniMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  AniMatchApp.swift
+//  AniMatch
+//
+//  Created by Tara Lim on 28/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct AniMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AniMatch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AniMatch
+//
+//  Created by Tara Lim on 28/3/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
