af2bca7f1f7000827b1d8c4922f9858d204b171f
Tara L <taralim826@gmail.com>
2024-05-22 09:44:19 +0800

    Created comment

--- AniMatch/ContentView.swift
+++ AniMatch/ContentView.swift
@@ -6,19 +6,149 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    
+    @State var characterList: [String] = ["Gon", "Kaori", "Mikasa", "Mononoke", "Mustang", "Nishinoya", "SpiritedAway", "Tomioka"]
+    @State var computerSlot: String = ""
+    @State var playerSlot: String = ""
+    @State var resultText: String = ""
+    @State var restartButton: Bool = false
+    @State var wins: Int = 0
+    @State var attempts: Int = 0
+    @State var showAlert: Bool = false
+    
+    func restartGame() {
+        for _ in characterList {
+            let computerRandom = Int.random(in: 0..<characterList.count)
+            computerSlot = characterList[computerRandom]
+            
+            let playerRandom = Int.random(in: 0..<characterList.count)
+            playerSlot = characterList[playerRandom]
         }
-        .padding()
+        
+        if computerSlot == playerSlot {
+            resultText = "✅ Correct Match ✅"
+            restartButton = true
+        } else {
+            resultText = ""
+            restartButton = false
+        }
+    }
+    
+    // Generate new characeter & check score
+    func newCharacter(character: inout String) {
+        while attempts < 30 && character != computerSlot {
+            
+            let randomInt = Int.random(in: 0..<characterList.count)
+            character = characterList[randomInt]
+            
+            if computerSlot == character {
+                resultText = "✅ Correct Match ✅"
+                restartButton = true
+                wins += 1
+                attempts += 1
+            } else {
+                attempts += 1
+                return resultText = "❌ Incorrect Match ❌"
+            }
+        }
+        if attempts == 30 {
+            showAlert = true
+        }
+    }
+    
+    var body: some View {
+        ZStack {
+            Image("Background")
+            
+            VStack {
+                HStack {
+                    Image(systemName: "bolt.horizontal.fill")
+                    Text("AniMatch")
+                        .font(.largeTitle)
+                        .fontWeight(.heavy)
+                        .fontDesign(.rounded)
+                    Image(systemName: "bolt.horizontal.fill")
+                }
+                
+                Text("Click 'SWITCH' to match the required image")
+                    .font(.headline)
+                    .fontWeight(.medium)
+                    .fontDesign(.rounded)
+                
+                Text("Maximum attempts is 30!")
+                    .font(.headline)
+                    .fontWeight(.medium)
+                    .fontDesign(.rounded)
+                
+                Image(computerSlot)
+                        .resizable()
+                        .frame(width: 200, height: 200)
+                        .padding()
+                
+                Text(resultText)
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .fontDesign(.rounded)
+                    .foregroundColor(.black)
+                
+                Text("Wins to Attempts: \(wins) : \(attempts)")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .fontDesign(.rounded)
+                    .foregroundColor(.black)
+                
+                if restartButton == true {
+                    Button(action: {
+                        restartGame()
+                    }, label: {
+                        Text("NEXT ROUND")
+                            .frame(width: 200, height: 30)
+                            .font(.title3)
+                            .fontWeight(.semibold)
+                            .fontDesign(.rounded)
+                            .foregroundColor(.black)
+                            .background(.red)
+                            .cornerRadius(20)
+                    })
+                } else {
+                    Text("")
+                        .frame(height: 30)
+                }
+                
+                Image(playerSlot)
+                        .resizable()
+                        .frame(width: 200, height: 200)
+                        .padding()
+                
+                Button(action: {
+                    newCharacter(character: &playerSlot)
+                }, label: {
+                    Text("SWITCH")
+                        .frame(width: 100, height: 50)
+                        .font(.title3)
+                        .fontWeight(.semibold)
+                        .fontDesign(.rounded)
+                        .foregroundColor(.black)
+                        .background(.red)
+                        .cornerRadius(20)
+                        .padding(.top)
+                })
+                .alert(isPresented: $showAlert) {
+                    Alert(
+                        title: Text("You reached the maximum tries"),
+                        message: Text("Come again tomorrow to play!"),
+                        dismissButton: .default(Text("OK"))
+                    )}
+
+            }
+            .padding()
+            .onAppear{
+                restartGame()
+            }
+        }
     }
 }
-
 #Preview {
     ContentView()
 }
